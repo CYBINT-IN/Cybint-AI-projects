6909e84a07f3f2ccd2d1620a9ad7f25b2ec95ccf
Tanmay Thakur <thakurtanmay72@yahoo.com>
2019-12-08 21:59:30 +0530

    Load Learner function implemented

--- DeepFashion/train_pytorch.py
+++ DeepFashion/train_pytorch.py
@@ -17,9 +17,14 @@ classes = ['Blouse', 'Blazer', 'Button-Down', 'Bomber', 'Anorak', 'Tee', 'Tank',
 single_img_data = ImageDataBunch.single_from_classes(path, classes, tfms=get_transforms(),
                                                      size=150).normalize(imagenet_stats)
 
-learn = create_cnn(single_img_data, models.resnet34)
+learn = create_cnn(single_img_data,models.resnet34)
 
 learn.load('stage-1_sz-150')
+
+"""
+# if you have export.pkl file, do this straight away
+learn = load_learner('data/cloth_categories/stage-1_sz-150')
+"""
 
 IMG_FILE_SRC =  path/"test_images/test-1.jpeg"
 
