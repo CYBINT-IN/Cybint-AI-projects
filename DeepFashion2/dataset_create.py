50b41d26c8931119707fec8fefdfbb8f936340d3
Tanmay Thakur <thakurtanmay72@yahoo.com>
2019-12-15 12:05:27 +0530

    Global Variables Changed

--- DeepFashion2/dataset_create.py
+++ DeepFashion2/dataset_create.py
@@ -23,7 +23,8 @@ from selective_search import selective_search_bbox
 
 # Global Variables
 max_categories=50
-category_name_generate=['Coat', 'Kaftan', 'Robe']
+category_name_generate=["Flannel","Poncho","Turtleneck","Capris","Cape","Coverup","Jumpsuit","Anorak","Bomber"]
+# category_name_generate=["Hoodie","Jacket","Sweater","Tank","Tee","Top","Blazer","Jeans","Joggers","Blouse","Leggings","Shorts","Skirt","Sweatpants","Coat","Dress","Jumpsuit","Kimono","Romper","Cardigan"]
 
 
 # In[4]:
@@ -155,8 +156,7 @@ def display_bbox(image_path_name, boxA, boxB):
 
     # load image
     img = skimage.io.imread(image_path_name)
-    logging.debug('img {}'.format(type(img)))
-
+    
     # Draw rectangles on the original image
     fig, ax = plt.subplots(ncols=1, nrows=1, figsize=(6, 6))
     ax.imshow(img)
@@ -165,16 +165,10 @@ def display_bbox(image_path_name, boxA, boxB):
     x, y, w, h = boxA[0], boxA[1], boxA[2]-boxA[0], boxA[3]-boxA[1]
     rect = mpatches.Rectangle((x, y), w, h, fill=False, edgecolor='green', linewidth=1)
     ax.add_patch(rect)
-    logging.debug('GT: boxA {}'.format(boxA))
-    logging.debug('   x    y    w    h')
-    logging.debug('{:4d} {:4d} {:4d} {:4d}'.format(x, y, w, h))
 
     x, y, w, h = boxB[0], boxB[1], boxB[2]-boxB[0], boxB[3]-boxB[1]
     rect = mpatches.Rectangle((x, y), w, h, fill=False, edgecolor='red', linewidth=1)
     ax.add_patch(rect)
-    logging.debug('boxB {}'.format(boxB))
-    logging.debug('   x    y    w    h')
-    logging.debug('{:4d} {:4d} {:4d} {:4d}'.format(x, y, w, h))
     
     plt.show()
 
@@ -183,16 +177,11 @@ def display_bbox(image_path_name, boxA, boxB):
 
 
 def calculate_bbox_score_and_save_img(image_path_name, dataset_image_path, gt_x1, gt_y1, gt_x2, gt_y2):
-
-    logging.debug('dataset_image_path {}'.format(dataset_image_path))
-    logging.debug('image_path_name {}'.format(image_path_name))
-
+    
     candidates = selective_search_bbox(image_path_name)
-    logging.debug('candidates {}'.format(candidates))
 
     image_name = image_path_name.split('/')[-1].split('.')[0]
-    logging.debug('image_name {}'.format(image_name))
-
+    
     img_read = Image.open(image_path_name)
     logging.debug('{} {} {}'.format(img_read.format, img_read.size, img_read.mode))
 
@@ -200,15 +189,11 @@ def calculate_bbox_score_and_save_img(image_path_name, dataset_image_path, gt_x1
     for x, y, w, h in (candidates):
         #  left, upper, right, and lower pixel; The cropped section includes the left column and
         #  the upper row of pixels and goes up to (but doesn't include) the right column and bottom row of pixels
-        logging.debug('Cropped image: i {}'.format(i))
         i=i+1
 
         boxA = (gt_x1, gt_y1, gt_x2, gt_y2)
         boxB = (x, y, x+w, y+h)
         iou = bb_intersection_over_union(boxA, boxB)
-        logging.debug('boxA {}'.format(boxA))
-        logging.debug('boxB {}'.format(boxB))
-        logging.debug('iou {}'.format(iou))
 
         # Uncomment only for testing as too much cpu/memory wastage
         #display_bbox(image_path_name, boxA, boxB)
@@ -218,27 +203,24 @@ def calculate_bbox_score_and_save_img(image_path_name, dataset_image_path, gt_x1
         image_save_name = image_path_name.split('/')[-2] + '_' + image_path_name.split('/')[-1].split('.')[0]
         image_save_path = dataset_image_path.rsplit('/', 1)[0]
         image_save_path_name = image_save_path + '/' + image_save_name + '_crop_' +  str(x) + '-' + str(y) + '-' + str(x+w) + '-' + str(y+h) + '_iou_' +  str(iou) + '.jpg'
-        logging.debug('image_save_path_name {}'.format(image_save_path_name))
         img_crop.save(image_save_path_name)
-        logging.debug('img_crop {} {} {}'.format(img_crop.format, img_crop.size, img_crop.mode))
 
     # Ground Truth
     image_save_name = image_path_name.split('/')[-2] + '_' + image_path_name.split('/')[-1].split('.')[0]
     image_save_path = dataset_image_path.rsplit('/', 1)[0]
     image_save_path_name = image_save_path + '/' + image_save_name + '_gt_' +  str(gt_x1) + '-' + str(gt_y1) + '-' + str(gt_x2) + '-' + str(gt_y2) + '_iou_' +  '1.0' + '.jpg'
-    logging.debug('image_save_path_name {}'.format(image_save_path_name))
     img_crop = img_read.crop((gt_x1, gt_y1, gt_x2, gt_y2))
     img_crop.save(image_save_path_name)
-    logging.debug('img_crop {} {} {}'.format(img_crop.format, img_crop.size, img_crop.mode))
 
 
 # In[12]:
 
 
 # Generate images from fashon-data into dataset
+from time import time
 def generate_dataset_images(category_names):
 
-
+    Tm=time()
     count=0
     with open(fashion_dataset_path + '/Anno/list_bbox.txt') as file_list_bbox_ptr:
         with open(fashion_dataset_path + '/Anno/list_category_img.txt') as file_list_category_img:
@@ -259,7 +241,6 @@ def generate_dataset_images(category_names):
                     logging.debug('image_category_index {}'.format(image_category_index))                       # 2
 
                     if category_names[image_category_index] not in category_name_generate:
-                        logging.debug('Skipping {} {}'.format(category_names[image_category_index], image_path_name))
                         continue
 
 
@@ -278,17 +259,10 @@ def generate_dataset_images(category_names):
                             logging.error('Unknown dataset_split_name {}'.format(dataset_image_path))
                             exit(1)
 
-                        logging.debug('image_category_index {}'.format(image_category_index))
-                        logging.debug('category_names {}'.format(category_names[image_category_index]))
-                        logging.debug('dataset_image_path {}'.format(dataset_image_path))
-
                         # Get ground-truth bounding boxes
                         gt_x1, gt_y1, gt_x2, gt_y2 = get_gt_bbox(image_path_name, file_list_bbox_ptr)                              # Origin is top left, x1 is distance from y axis;
                                                                                                                                    # x1,y1: top left coordinate of crop; x2,y2: bottom right coordinate of crop
-                        logging.debug('Ground bbox:  gt_x1:{} gt_y1:{} gt_x2:{} gt_y2:{}'.format(gt_x1, gt_y1, gt_x2, gt_y2))
-
                         image_path_name_src = os.path.join(fashion_dataset_path, 'Img', image_path_name)
-                        logging.debug('image_path_name_src {}'.format(image_path_name_src))
 
                         calculate_bbox_score_and_save_img(image_path_name_src, dataset_image_path, gt_x1, gt_y1, gt_x2, gt_y2)
 
@@ -296,10 +270,9 @@ def generate_dataset_images(category_names):
                         #shutil.copyfile(os.path.join(fashion_dataset_path, 'Img', image_path_name), dataset_image_path)
 
                         idx_crop = idx_crop + 1
-                        logging.debug('idx_crop {}'.format(idx_crop))
 
                     count = count+1
-                    logging.info('count {} {}'.format(count, dataset_image_path))
+    print("Dataset creation over. Time taken ",time()-Tm," s")
 
 
 # In[13]:
@@ -343,7 +316,7 @@ create_category_structure(category_names)
 generate_dataset_images(category_names)
 
 
-# In[19]:
+# In[ ]:
 
 
 display_category_data()
