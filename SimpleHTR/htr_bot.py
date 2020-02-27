3061b5cd3faef355f47aa874313a9fe06dccdc49
Tanmay Thakur <thakurtanmay72@yahoo.com>
2020-02-27 18:54:18 +0530

    Bot updated to use pytesseract

--- SimpleHTR/htr_bot.py
+++ SimpleHTR/htr_bot.py
@@ -4,31 +4,18 @@ Created on Sun Sep 15 00:00:12 2019
 
 @author: tanma
 """
-import os
+import cv2
 import logging
+import pytesseract
 import urllib.request
 
-from segment import segment_main
-from telegram import ChatAction
 from telegram.ext import Updater, MessageHandler, Filters
-
-from DataLoader import DataLoader, Batch
-from Model import Model, DecoderType
-from SamplePreprocessor import preprocess
-from autocorrect import Speller
-from main import infer, FilePaths
-
-
-spell = Speller(lang = "en")
-
-decoderType = DecoderType.BestPath
-loader = DataLoader(FilePaths.fnTrain, Model.batchSize, Model.imgSize, Model.maxTextLen)
-model = Model(loader.charList, decoderType) 
-
+ 
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
 
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 def download_image(url,name):
     fullname = str(name)+".jpeg"
@@ -38,13 +25,13 @@ def download_image(url,name):
 def some_func(bot, update):
     pass
     if not update.effective_message.photo:
-        update.effective_message.reply_text(text = "This bot is only capable of Computer Vision Tasks!")
+        update.effective_message.reply_text(text = "This bot is only capable of Computer Vision Tasks! Getting self-aware soon!")
     else:
         msg = update.effective_message
         file_id = msg.photo[-1].file_id
         photo = bot.get_file(file_id)
         download_image(photo["file_path"],'wassup')
-        text = infer(model, 'wassup.jpeg')
+        text = pytesseract.image_to_string(cv2.imread("wassup.jpeg"), lang = 'eng')
         update.effective_message.reply_text(text = text)
 
         
